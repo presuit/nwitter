8a2c09b39f90273ae367074bc3b956e9dab23983
Yeongho Choi <chldudgh8256@naver.com>
2020-09-15 21:50:14 +0900

    Updates

---
+++ precache-manifest.0743a634569437a7b81b77fce21cedf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c52613e7a0d7d10e44499a9c6f13df",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "8c067e62a324b593cc3a",
+    "url": "/nwitter/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "dc7ea85eee21d9d1c9bf",
+    "url": "/nwitter/static/css/main.80ed51e6.chunk.css"
+  },
+  {
+    "revision": "8c067e62a324b593cc3a",
+    "url": "/nwitter/static/js/2.ef0391e0.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/nwitter/static/js/2.ef0391e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc7ea85eee21d9d1c9bf",
+    "url": "/nwitter/static/js/main.ec0d507b.chunk.js"
+  },
+  {
+    "revision": "e8911b8f91ff3a390ea9",
+    "url": "/nwitter/static/js/runtime-main.aba69e0a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.615f4e5901e727610d564d036822c659.js"
+  "/nwitter/precache-manifest.0743a634569437a7b81b77fce21cedf6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nwitter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
